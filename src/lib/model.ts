2c059537f187d0a83281e96dadaad0043adf368e
altapia <altapia@gmail.com>
2024-03-02 12:40:24 +0100

    primera versión

---
+++ src/lib/model.ts
@@ -0,0 +1,25 @@
+export interface User {
+    id: number,
+    nombre: string,
+    team: Team,
+    apuestas?: Apuesta[]
+}
+export interface Team {
+    id: number,
+    nombre: string,
+    users?: User[]
+}
+
+export interface Estado {
+    id: number,
+    nombre: string
+}
+
+export interface Apuesta {
+    id: number,
+    user: User,
+    descripcion: string,
+    importe: number,
+    cuota: number,
+    estado: number
+}
---
+++ src/lib/teams.ts
@@ -0,0 +1,28 @@
+import type { Team } from "./model";
+
+export const equipos: Team[] = [
+    {
+        id: 1,
+        nombre: 'Team 1'
+    },
+    {
+        id: 2,
+        nombre: 'Team 2'
+    },
+    {
+        id: 3,
+        nombre: 'Team 3'
+    },
+    {
+        id: 4,
+        nombre: 'Team 4'
+    },
+    {
+        id: 5,
+        nombre: 'Team 5'
+    },
+    {
+        id: 6,
+        nombre: 'Team 6'
+    },
+]
