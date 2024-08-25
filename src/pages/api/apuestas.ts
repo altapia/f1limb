d8b7cd4603982967ba746af945849a0f5ea67c15
altapia <altapia@gmail.com>
2024-08-25 18:29:34 +0200

    api GEP apuestas

---
+++ src/pages/api/apuestas.ts
@@ -0,0 +1,45 @@
+import type { APIRoute } from "astro"
+import { GpService } from "@/lib/gpService"
+import { ApuestaService } from "@/lib/apuestaService"
+import type { ApuestaVO, UserVO } from "@/lib/model"
+import { UserService } from "@/lib/userService"
+
+export const GET: APIRoute = async ({ params, request }) => {
+	const gpService = new GpService()
+	const gp = await gpService.getCurrent()
+	console.log(gp)
+	if (!gp || !gp.id) {
+		return new Response(null, {
+			status: 404,
+			statusText: "Not found",
+		})
+	}
+
+	const apuestaService = new ApuestaService()
+	const visible = await apuestaService.hanApostadoTodosTodo(gp.id, true)
+
+	let listApuestas: ApuestaVO[] = []
+	let usuarios: UserVO[] = []
+
+	if (!visible) {
+		listApuestas = await apuestaService.getApuestasByGP(gp.id)
+
+		let userService = new UserService()
+		usuarios = await userService.getUsersTeam()
+
+		usuarios.forEach((u) => {
+			u.apuestas = []
+			if (listApuestas !== undefined) {
+				let apuestasUsuario = listApuestas.filter((a) => a.user?.id === u.id)
+				u.apuestas = apuestasUsuario
+			}
+		})
+	}
+
+	return new Response(JSON.stringify(usuarios), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
+}
